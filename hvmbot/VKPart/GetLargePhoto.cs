a542a083962db31c3fff2dbb2192a4915051b3fb
Vladimir Kiselev <35545041+rhiskey@users.noreply.github.com>
2020-10-02 00:54:25 +0300

    Update GetLargePhoto.cs

--- hvmbot/VKPart/GetLargePhoto.cs
+++ hvmbot/VKPart/GetLargePhoto.cs
@@ -10,23 +10,17 @@ namespace hvmbot.VKPart
     {
         public static Photo GetLargeAndThumbUrl(List<VkNet.Model.Attachments.Photo > photoList)
         {
-            //var getLargePhoto = function(photo) {
-            //    return photo.photo_2560 || photo.photo_1280 || photo.photo_807 || photo.photo_604 || photo.photo_130 || photo.photo_75;
-            //};
             Uri largePhoto = null;
-            //thumbUrl = null;
             List<Uri> getLargePhoto = new List<Uri>();
             Uri thumbUrl = null;
-            //Ищем самую большую фотку
+            //Find Largest photo
             foreach (var photo in photoList)
             {
                 ulong? maxHeight = 0, maxWidth = 0, minWidth = 200;
 
-
                 foreach (var size in photo.Sizes)
                 {
-
-                    var h = size.Height; //Current
+                    var h = size.Height;
                     var w = size.Width;
 
                     //Get Largest
@@ -40,18 +34,8 @@ namespace hvmbot.VKPart
                     if (w <= minWidth && w > 75)
                     {
                         minWidth = w;
-
-                        ////Не работает пока отправка (почему-то не грузит фотку), надо её скачать и загрузить
                         thumbUrl = size.Url;
                     }
-                    ////Если картинка вертикальная
-                    //if (h > maxHeight)
-                    //    maxHeight = h;
-
-
-                    //if (size.Type == PhotoSizeType.M) //либо S
-                    //    if (size.Url != null)
-                    //        thumbUrl = size.Url;
                 }
                 getLargePhoto.Add(largePhoto);
             }
